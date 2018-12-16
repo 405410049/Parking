7bcdf6012dfcc739925b3a828ed569e9e8942adb
yoga_lai <lyc105u@csie0.cs.ccu.edu.tw>
2018-12-16 15:53:25 +0800

    2018/12/16

--- login.php
+++ login.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>停車場管理系統</title>
+	<link rel=stylesheet type="text/css" href="login.css">
 </head>
 <body>
     <form action="manage/index.php" method="post">
@@ -10,8 +11,8 @@
         <input type="text" name="Username" id="Username" required="required" /><br />
         <label for="Password">密碼</label>
         <input type="password" name="Password" id="Password" required="required" /><br />
-        <a href="register.php">註冊</a>
-        <input type="submit" name="Submit" value="登入">
+        <a href="register.php" id=reg>註冊</a>
+        <input type="submit" name="Submit" value="登入" id="submit_btn">
     </form>
 </body>
 </html>
--- register.php
+++ register.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="utf-8">
     <title>停車場管理系統</title>
+	<link rel=stylesheet type="text/css" href="register.css">
 </head>
-<body>
+<body id="content">
+	<h2>註冊帳戶</h1>
     <form action="register-check.php" method="post">
         <label for="Username">帳號</label>
         <input type="text" name="Username" id="Username" required="required" /><br />
@@ -12,7 +14,7 @@
         <input type="password" name="Password1" id="Password1" required="required" /><br />
         <label for="Password">確認密碼</label>
         <input type="password" name="Password2" id="Password2" required="required" /><br />
-        <input type="submit" name="Submit" value="立即註冊">
+        <input id="btn" type="submit" name="Submit" value="立即註冊">
     </form>
 </body>
 </html>
